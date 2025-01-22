e4949a7f3d818ae5dd48c3d4cf700795e7d2345c
yunhaoj99 <yunhaoj666@gmail.com>
2025-01-21 19:23:32 -0800

    Initial commit

---
+++ enhanced_dnc.py
@@ -0,0 +1,39 @@
+import sys
+from itertools import combinations
+import math
+from a1_utils import read_input_from_cli, distance, write_output_to_file
+
+
+def enhanced_divide_and_conquer_closest_pair(points: list[tuple[float, float]]) -> tuple[float, list[tuple[tuple[float, float], tuple[float, float]]]]:
+    """
+    Recursively find the closest pair of points using a divide-and-conquer approach.
+    
+    Args:
+        points (list[tuple[float, float]]): A list of 2D points where 
+                                            each point is represented as a tuple (x, y).
+                                            
+    Returns:
+        tuple[float, list[tuple[tuple[float, float], tuple[float, float]]]]:
+            - The minimum distance between the closest pair(s) of points.
+            - A list of tuples representing the closest point pairs, where each pair is a 
+              tuple of two points ((x1, y1), (x2, y2)).
+    """
+    
+    # TODO 
+
+      return distance, pairs
+
+
+if __name__ == "__main__":
+    try:
+        points = read_input_from_cli()
+        min_dist, closest_pairs = enhanced_divide_and_conquer_closest_pair(points)
+
+        print(f"Minimum Distance: {min_dist}")
+        print("Closest Pairs:")
+        for pair in closest_pairs:
+            print(pair)
+        write_output_to_file(distance=min_dist, points=closest_pairs, output_file= 'enhance_ddnc_output.txt')
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
